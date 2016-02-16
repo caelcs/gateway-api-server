c5de59d8ab13957ba3e844df33f00253a643dd4b
caelwinner <adolfoecs@gmail.com>
2016-02-16 23:03:39 +0000

    get token automatic and inject it into the request to api

--- src/main/java/uk/co/caeldev/gateway/api/config/CommonConfiguration.java
+++ src/main/java/uk/co/caeldev/gateway/api/config/CommonConfiguration.java
@@ -2,13 +2,10 @@ package uk.co.caeldev.gateway.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@ImportResource({"classpath:META-INF/spring/integration/identity-oauth2-context.xml",
-        "classpath:META-INF/spring/integration/registration-context.xml"})
-public class IntegrationConfiguration {
+public class CommonConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
